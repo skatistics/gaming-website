55ac3cb7c1db8c5ad198f572c1f055bdadd31429
skatistics <131618960+skatistics@users.noreply.github.com>
2024-09-21 04:02:46 +0800

    testing useState
    useState fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 function App() {
-  const [products, setProducts] = useState(null);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products').then((res) => {return res.json()}).then((data)=>{setProducts(data)});
-  }, [])
+    fetch('https://fakestoreapi.com/products').then((res) => {return res.json();}).then((data)=>{console.log(data); setProducts(data);});
+  }, []);
 
   return (
     products && (
       <div>
         {
-          products.map((product)=>{
-            <div>
-              ANG UNANG ITEM AY { product.title }
-            </div>
+          products.map((product)=>{ return (<h1 key={ product.id }>
+            ANG UNANG ITEM AY { product.title }
+          </h1>)
+            
           })
         }
       </div>
